f643b47358a6aa65c8fdddd80a59c2daf435a7ca
Purav Shah <pshah116@myseneca.ca>
2025-05-20 19:29:15 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Purav Shah
+# Author ID: pshah116
+# Date Created: Tuesday May 20,2025
+
+count = 10
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("blast off!")
