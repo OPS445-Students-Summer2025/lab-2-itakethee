475c5b03d2dca63d1ca1d4a3f4e71d4cf5aef305
Purav Shah <pshah116@myseneca.ca>
2025-05-20 19:33:35 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+# Author: Purav Shah
+# Author ID: pshah116
+# Date Created: Tuesday May 20,2025
+
+count = int(sys.argv[1])
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("blast off!")
